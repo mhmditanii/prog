3f9f2baebff700d934774fa4e022307e461f1abf
mhmditani <mhmd04itani@gmail.com>
2025-03-28 15:11:39 +0100

    removed the nested json library and adjuested the code likewise

--- task-2/main.py
+++ task-2/main.py
@@ -2,18 +2,16 @@ import tkinter as tk
 import os
 import json
 
-
-# searches for the file and return None if file does not exist
 def find(name, path):
     for root, dirs, files in os.walk(path):
         if name in files:
             return os.path.join(root, name)
     return None
 
-def load_data(current_file):
-    return json.load(current_file)
+def load_data():
+    with open_file('r') as current_file:
+        return json.load(current_file)
 
-# takes care of file handling errors
 def open_file(access_type):
     try:
         file_path = find("library_data.json", os.getcwd())
@@ -27,41 +25,43 @@ def open_file(access_type):
 
 def save_file(library_data):
     with open_file('w') as library:
-        json.dump(library_data, library, indent = 4)
+        json.dump(library_data, library, indent=4)
 
 def list_books():
-    with open_file('r') as library:
-        library_data = json.load(library)
-        for book in library_data.values():
-            for details in book:  
-                print(f"Title: {details['title']}")
-                print(f"Author: {details['author']}")
-                print(f"Year: {details['year']}")
-                print('\n')
+    library_data = load_data()
+    for book in library_data:
+        print(f"Title: {book['title']}")
+        print(f"Author: {book['author']}")
+        print(f"Year: {book['year']}")
+        print('\n')
 
 def add_book(book_details):
-    with open_file('r') as library:
-        library_data = load_data(library)
-    if any(book_details[0] == book["title"] for books in library_data.values() for book in books):
-        print("Book is already found!")
-        return
+    library_data = load_data()
+    for book in library_data:
+        if book_details[0] == book["title"]:
+            print("Book is already found!")
+            return 
     entry = {
         "title": book_details[0],
         "author": book_details[1],
         "year": book_details[-1]
     }
-    author = book_details[1]
-    if author in library_data:
-        library_data[author].append(entry)
-    else:
-        library_data[author] = [entry]
+    library_data.append(entry)
     save_file(library_data)
     print("Book added successfully")
 
+def delete_book(book_name):
+    library_data = load_data()
+    for book in library_data:
+        if book_name == book["title"]:
+            library_data.remove(book)
+            save_file(library_data)
+            print(f"Book '{book_name}' deleted successfully")
+            return
+    print(f"Book '{book_name}' not found!")
 
+add_book(("test","test1", 1222))
 
-# Opening the file
+delete_book("test")
 
 list_books()
-#add_book(("Zorba : The Greek", "Nikos Kazantzakis", 1946))
-#print(data)
